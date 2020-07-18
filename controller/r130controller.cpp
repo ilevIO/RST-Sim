8757dc891e143f178be2228647976f945c2f75c9
ilevIO <yelagov@gmail.com>
2020-07-18 20:42:29 +0300

    Refined

---
+++ controller/r130controller.cpp
@@ -0,0 +1,6 @@
+#include "r130controller.h"
+
+R130controller::R130controller()
+{
+
+}
---
+++ controller/r130controller.h
@@ -0,0 +1,11 @@
+#ifndef R130CONTROLLER_H
+#define R130CONTROLLER_H
+#include "r123controller.h"
+
+class R130controller
+{
+public:
+    R130controller();
+};
+
+#endif // R130CONTROLLER_H
